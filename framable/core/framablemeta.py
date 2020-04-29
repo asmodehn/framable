c103e20940a229b06bed0fa3974c81b407c35e25
AlexV <asmodehn@gmail.com>
2020-04-29 16:26:05 +0200

    merging base behavior and underlying tuple implementation all into framablemeta code...

--- framable/core/framablemeta.py
+++ framable/core/framablemeta.py
@@ -1,18 +1,82 @@
+import collections
+import sys
+
 import pandas as pd
 
 
 # TODO : record the object address or full name, for reference in "foreign keys" later...
 
+# _prohibited = ['__new__',]
+
+
 class FramableMeta(type):
     def __new__(mcls, name, bases, ns):
 
-        cls = super(FramableMeta, mcls).__new__(mcls, name, bases, ns)
-        return cls
+        # If called dynamically, some mandatory attributes might be missing:
+
+        # __module__ (source inspired from collections.namedtuple)
+
+        # For pickling to work, the __module__ variable needs to be set to the frame
+        # where the named tuple is created.  Bypass this step in environments where
+        # sys._getframe is not defined (Jython for example) or sys._getframe is not
+        # defined for arguments greater than 0 (IronPython), or where the user has
+        # specified a particular module.
+        if ns.get('__module__') is None:
+            try:
+                module = sys._getframe(1).f_globals.get('__name__', '__main__')
+            except (AttributeError, ValueError):
+                pass
+            ns.setdefault('__module__', module)
+
+        #TODO: __qualname__
+
+        # We have to build a subclass of the namedtuple
+        # inspired from typing.NamedTupleMeta : https://github.com/python/cpython/blob/master/Lib/typing.py#L1731
+        types = ns.get('__annotations__', {})
+        default_names = []
+        for field_name in types:
+            if field_name in ns:
+                default_names.append(field_name)
+            elif hasattr(types[field_name], '__initial__'):  # assigning the initial element as default
+                ns[field_name] = types[field_name].__initial__
+                default_names.append(field_name)
+            else:
+                raise TypeError(f"Non-framable type {types[field_name]} of field {field_name} "
+                                f"needs to specify a default value")
+
+        Impl = collections.namedtuple(typename=f"{name}Impl", field_names=types.keys(),
+                                      defaults=[ns[n] for n in default_names],
+                                      module=ns['__module__'])
+        Impl.__annotations__ = types
+
+        return super(FramableMeta, mcls).__new__(mcls, name, bases + (Impl,), ns)
 
     def __init__(cls, name, bases, ns):
+        # leveraging inheritance for implementation, for simplicity reasons
         super(FramableMeta, cls).__init__(name, bases, ns)
-        cls.__initial__ = cls()  # creating initial object on class creation
+
+        # overriding __init__ for additional behavior (__new__ is reserved for namedtuple)
+        def init(s, *args, **kwargs):
+            super(cls, s).__init__()
+            for f in s._fields:
+                # replacing instance values with values from tuple (immutable) for consistency
+                setattr(s, f, getattr(super(cls, s), f))
+
+        cls.__init__ = init
+
         cls._classframe = pd.DataFrame()  # empty dataframe on class creation
+
+        cls.__initial__ = cls()  # creating initial object on class creation
+
+        # setting up property to represent instance as a series
+        def series(self):
+            if isinstance(self, dict):
+                return pd.Series(self)
+            elif isinstance(self, tuple) and hasattr(self, "_asdict"):
+                return pd.Series(self._asdict())
+            else:
+                return pd.Series(vars(self))
+        cls.__series__ = property(series)
 
     def __call__(cls, *args, **kwargs):
         if not args and not kwargs:
@@ -47,14 +111,10 @@ class FramableMeta(type):
 if __name__ == "__main__":
 
     class MyKls(metaclass=FramableMeta):
+        att1: int = 0
+        att2: int = 42
 
-        att1: int
-
-        def __init__(self, att1, att2):
-            self.att1 = att1
-            self.att2 = att2  # unspecified in class, added on init
-
-    myobj = MyKls(42, 51)
+    myobj = MyKls(att1=42, att2=51)
 
     print(MyKls.__frame__)
 
@@ -65,6 +125,10 @@ if __name__ == "__main__":
             "expected AttributeError, as the instance doesnt have a frame representation provided by the metaclass"
         )
 
-    myobj2 = MyKls(47, 53)
+    print(myobj.__series__)
+
+    myobj2 = MyKls(att1=47, att2=53)
 
     print(MyKls.__frame__)
+
+    print(myobj2.__series__)
--- tests/core/test_framablemeta.py
+++ tests/core/test_framablemeta.py
@@ -8,20 +8,21 @@ from framable.core.framablemeta import FramableMeta
 class TestFramableMeta(unittest.TestCase):
     def test_framableclass(self):
         class MyKls(metaclass=FramableMeta):
-
-            att1: int
-
-            def __init__(self, att1=0, att2=0):
-                self.att1 = att1
-                self.att2 = att2  # unspecified in class, added on init
+            att1: int = 0
+            att2: int = 42
 
         assert isinstance(MyKls.__frame__, pd.DataFrame)
         assert MyKls.__frame__.empty
         assert len(MyKls.__frame__) == 0
 
-        myobj = MyKls(42, 51)
+        myobj = MyKls(att1=42, att2=51)
 
-        assert len(MyKls.__frame__) == 1
+        assert myobj.att1 == 42
+        assert myobj.att2 == 51
+
+        # the class has a frame (from the metaclass)
+        assert (MyKls.__frame__ == pd.DataFrame([vars(myobj)])).all().all()
+        # or checking by column
         assert (MyKls.__frame__.columns == ["att1", "att2"]).all()
         # proper panda way to check datatype...
         assert pd.api.types.is_int64_dtype(MyKls.__frame__.dtypes["att1"])
@@ -29,37 +30,68 @@ class TestFramableMeta(unittest.TestCase):
         # checking the first record is the instance we just created
         assert (MyKls.__frame__.iloc[0] == pd.Series(vars(myobj))).all()
 
+        # but the obj instance doesnt have the attribute
         with self.assertRaises(AttributeError):
             myobj.__frame__
         # expected AttributeError, as the instance doesnt have a frame representation provided by the class
 
         # appending happens as expected
-        myobj2 = MyKls(47, 53)
+        myobj2 = MyKls(att1=47, att2=53)
+
+        assert myobj2.att1 == 47
+        assert myobj2.att2 == 53
 
         # checking the second record is the instance we just created
         assert (MyKls.__frame__.iloc[1] == pd.Series(vars(myobj2))).all()
 
+        # Which is reflected in its series
+        assert (myobj2.__series__ == pd.Series(vars(myobj2))).all()
+
         # Test with another class in an attempt to detect conflicts...
         class MyOtherKls(metaclass=FramableMeta):
 
-            att1: str
-
-            def __init__(self, att1=None, att2=0):
-                self.att1 = att1 if att1 is not None else ""
-                self.att2 = att2  # unspecified in class, added on init
+            att1: str = "dflt"
+            att2: int = 0
 
         assert isinstance(MyOtherKls.__frame__, pd.DataFrame)
         assert MyOtherKls.__frame__.empty
 
-        myotherobj = MyOtherKls("alice", 51)
+        myotherobj = MyOtherKls(att1="alice", att2=51)
 
-        assert len(MyOtherKls.__frame__) == 1
+        assert (MyOtherKls.__frame__ == pd.DataFrame([vars(myotherobj)])).all().all()
         assert (MyOtherKls.__frame__.columns == ["att1", "att2"]).all()
         # proper panda way to check datatype...
         assert pd.api.types.is_string_dtype(MyOtherKls.__frame__.dtypes["att1"])
         assert pd.api.types.is_int64_dtype(MyOtherKls.__frame__.dtypes["att2"])
         # checking the first record is the instance we just created
         assert (MyOtherKls.__frame__.iloc[0] == pd.Series(vars(myotherobj))).all()
+
+    def test_framablecall(self):
+
+        MyTpl = FramableMeta("MyTpl", (), {
+            "att1": 0,
+            "att2": 42,
+            "__annotations__":{
+                "att1": int, "att2": int
+            }})
+
+        myobj = MyTpl(att1=42, att2=51)
+
+        assert (myobj.__series__ == pd.Series(myobj._asdict())).all()
+
+        # the class has a frame (from the metaclass)
+        assert (MyTpl.__frame__ == pd.DataFrame([myobj._asdict()])).all().all()
+        # but the obj instance doesnt have the attribute
+        with self.assertRaises(AttributeError):
+            myobj.__frame__
+        # expected AttributeError, as the instance doesnt have a frame representation provided by the class
+
+        # appending happens as expected
+        myobj2 = MyTpl(att1=47, att2=53)
+
+        # checking the second record is the instance we just created
+        assert (myobj2.__series__ == pd.Series(myobj2._asdict())).all()
+
 
 
 if __name__ == "__main__":
